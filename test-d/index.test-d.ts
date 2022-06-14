1780b150ad8ed4aab3f24bea2443fc9671ee3c2b
Georgios Tsakonas <94357854+geotsakonas@users.noreply.github.com>
2022-06-14 13:04:01 +0300

    Update index.test-d.ts

--- test-d/index.test-d.ts
+++ test-d/index.test-d.ts
@@ -22,6 +22,78 @@ mem((_arguments: {key: string}) => 1, {
 		set: (key, data) => {
 			expectType<Date>(key);
 			expectType<{data: number; maxAge: number}>(data);
+			
+			mem(() => 1, {
+	cacheKey: arguments_ => {
+		expectType<[]>(arguments_); // eslint-disable-line @typescript-eslint/ban-types
+	},
+});
+
+// Ensures that the various cache functions infer their arguments type from the return type of `cacheKey`
+mem((_arguments: {key: string}) => 1, {
+	cacheKey: (arguments_: [{key: string}]) => {
+		expectType<[{key: string}]>(arguments_);
+		return new Date();
+	},
+	cache: {
+		get: key => {
+			expectType<mem(() => 1, {
+	cacheKey: arguments_ => {
+		expectType<[]>(arguments_); // eslint-disable-line @typescript-eslint/ban-types
+	},
+});
+
+// Ensures that the various cache functions infer their arguments type from the return type of `cacheKey`
+mem((_arguments: {key: string}) => 1, {
+	cacheKey: (arguments_: [{key: string}]) => {
+		expectType<[{key: string}]>(arguments_);
+		return new Date();
+	},
+	cache: {
+		get: key => {
+			expectType<mem(() => 1, {
+	cacheKey: arguments_ => {
+		expectType<[]>(arguments_); // eslint-disable-line @typescript-eslint/ban-types
+	},
+});
+
+// Ensures that the various cache functions infer their arguments type from the return type of `cacheKey`
+mem((_arguments: {key: string}) => 1, {
+	cacheKey: (arguments_: [{key: string}]) => {
+		expectType<[{key: string}]>(arguments_);
+		return new Date();
+	},
+	cache: {
+		get: key => {
+			expectType<mem(() => 1, {
+	cacheKey: arguments_ => {
+		expectType<[]>(arguments_); // eslint-disable-line @typescript-eslint/ban-types
+	},
+});
+
+// Ensures that the various cache functions infer their arguments type from the return type of `cacheKey`
+mem((_arguments: {key: string}) => 1, {
+	cacheKey: (arguments_: [{key: string}]) => {
+		expectType<[{key: string}]>(arguments_);
+		return new Date();
+	},
+	cache: {
+		get: key => {
+			expectType<mem(() => 1, {
+	cacheKey: arguments_ => {
+		expectType<[]>(arguments_); // eslint-disable-line @typescript-eslint/ban-types
+	},
+});
+
+// Ensures that the various cache functions infer their arguments type from the return type of `cacheKey`
+mem((_arguments: {key: string}) => 1, {
+	cacheKey: (arguments_: [{key: string}]) => {
+		expectType<[{key: string}]>(arguments_);
+		return new Date();
+	},
+	cache: {
+		get: key => {
+			expectType<
 		},
 		has: key => {
 			expectType<Date>(key);
